fixed output not on for keithly, also removed averaging

take_current.py
@@ -76,7 +76,7 @@ num_images = 1 # how many repeats
 keithly_input_channel = 1 # 0 = A, 1 = B
 current_step = 0.01 # amps
     
-savepath = r"C:\Users\akashdasgupta\Documents\EL\first_data\test4"
+savepath = r"C:\Users\akashdasgupta\Documents\EL\first_data\test6"
 whitepath= savepath+ r"\white"
 
 #*************************************************** 
@@ -105,6 +105,7 @@ else:
     kchan = k.smua
 
 kchan.reset()
+kchan.source.output = kchan.OUTPUT_ON
 # Not sure if I need this, setting to open circuit:
 kchan.source.func = kchan.OUTPUT_DCVOLTS
 kchan.source.levelv = 0 
@@ -132,11 +133,6 @@ def int_sweep_oc(vstep, num_snaps, savepath):
     if not os.path.isdir(savepath+'\\refs'):
         os.makedirs(savepath+'\\refs')
     
-    # Set 0V for open circuit
-    kchan.reset()
-    kchan.source.func = kchan.OUTPUT_DCVOLTS
-    kchan.source.levelv = 0 
-
     nominal_voltages = []
     source_currents = []
 
@@ -147,48 +143,46 @@ def int_sweep_oc(vstep, num_snaps, savepath):
     num_refs = 0
     for i in range(10):
         image = cam.snap()
+        #]
         imageio.imwrite(savepath+'\\refs'+"\\ref_"+str(num_refs)+".tif", image)
         num_refs += 1
 
     ps.write("OUTPUT ON")
-    for nominal_v in np.arange(2.9, 3.9,vstep):
+    for nominal_v in np.arange(2.6, 3.9,vstep):
         ps.write(f"VOLT {nominal_v}")
-        time.sleep(1)
+        time.sleep(2)
+        vm = kchan.measure.v()
+        im = kchan.measure.i()
+
 
         for i in range(num_snaps):
-            vm1 = kchan.measure.v()
-            im1 = kchan.measure.i()
             image = cam.snap()
             imageio.imwrite(savepath + "\\" + str(nominal_v)+"FORWARDS_"+str(i)+".tif", image)
-            vm2 = kchan.measure.v()
-            im2 = kchan.measure.i()
             image_index.append( str(nominal_v)+"FORWARDS_"+str(i)+".tif")
-            voltage.append((vm1+vm2)/2)
-            current.append((im1+im2)/2)
+            voltage.append(vm)
+            current.append(im)
 
-        im = measure_ps_iv()
+        ism = measure_ps_iv()
 
-        source_currents.append(im)
+        source_currents.append(ism)
         nominal_voltages.append(nominal_v)
     
-    for nominal_v in np.arange(3.85,2.8,-vstep):
+    for nominal_v in np.arange(3.9,2.6,-vstep):
         ps.write(f"VOLT {nominal_v}")
-        time.sleep(1)
+        time.sleep(2)
+        vm = kchan.measure.v()
+        im = kchan.measure.i()
 
         for i in range(num_snaps):
-            vm1 = kchan.measure.v()
-            im1 = kchan.measure.i()
             image = cam.snap()
             imageio.imwrite(savepath + "\\" + str(nominal_v)+"BACKWARDS"+str(i)+".tif", image)
-            vm2 = kchan.measure.v()
-            im2 = kchan.measure.i()
             image_index.append( str(nominal_v)+"BACKWARDS"+str(i)+".tif")
-            voltage.append((vm1+vm2)/2)
-            current.append((im1+im2)/2)
+            voltage.append(vm)
+            current.append(im)
 
-        im = measure_ps_iv()
+        ism = measure_ps_iv()
 
-        source_currents.append(im)
+        source_currents.append(ism)
         nominal_voltages.append(nominal_v)
     
     ps.write("OUTPUT OFF") # for safety