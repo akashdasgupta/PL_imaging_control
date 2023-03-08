d22f355c613a2c562c644925d493f9d7c28b3d84
Akash Dasgupta <akashdg09@gmail.com>
2023-03-08 11:35:16 +0000

    look for 'ardunio' in device description rather than s/n

--- generate_resource_strings.py
+++ generate_resource_strings.py
@@ -5,7 +5,7 @@ import csv
 #### Configure your specific setup here: 
 resource_strings = {'Keithley Instruments Inc., Model 2636, 1211652, 1.4.2': '\n', # Keithley ID
                     'FARNELL,MP710086,2017217,FV:V1.5.2': '\n', # Power supply ID
-                    '5&32CF30CB&0&7':' '} # Ardunio serial number
+                    'Arduino Micro':' '} # Ardunio serial number
 
 
 # Checks for scientific equipment
@@ -27,17 +27,15 @@ for inst_id in resource_strings.keys():
             break
         
         device.close()
-   
 
 # Checks for ardunio
 ports = sp.comports()
 for port in ports:
-    if port.serial_number in resource_strings.keys():
-        resource_strings[port.serial_number] = port.device
+    if'Arduino Micro' in port.description:
+        resource_strings['Arduino Micro'] = port.device
         
 # Writes strings: 
 with open('Resource_strings.csv', 'w', newline='') as csv_file:  
     writer = csv.writer(csv_file)
     for key, (_, value) in zip(['Source_meter', 'LED_power', 'Ardunio_mux'], resource_strings.items()):
        writer.writerow([key, value])
-        
